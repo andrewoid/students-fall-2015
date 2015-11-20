bc195656ef22f0ddd8b287cb403ae9c01d7fadbc
Andrew Schwimmer <andrew.schwimmer@gmail.com>
2015-11-19 22:17:50 -0500

    connect four

---
+++ schwimmer-mco152/src/schwimmer/pi/CalculatePi.java
@@ -0,0 +1,11 @@
+package schwimmer.pi;
+
+public class CalculatePi {
+	
+	public static void main(String[] args) {
+		PiCalculator calc = new PiCalculator();
+		
+		System.out.println(calc.calculate(100000000000L));
+	}
+
+}
---
+++ schwimmer-mco152/src/schwimmer/ufo/UFOSightingMain.java
@@ -0,0 +1,25 @@
+package schwimmer.ufo;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.google.gson.Gson;
+
+public class UFOSightingMain {
+
+	public static void main(String[] args) throws IOException {
+
+		final BufferedReader in = new BufferedReader(new FileReader("./ufo_awesome.json"));
+
+		final Gson gson = new Gson();
+
+		final UFOSighting array[] = gson.fromJson(in, UFOSighting[].class);
+
+		System.out.println(array.length);
+
+		in.close();
+
+	}
+
+}
